5d111b8e71eae8977f343acd2eae37ac40c0b7b4
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-09-20 11:20:54 -0600

    update

--- starter/script.js
+++ starter/script.js
@@ -234,21 +234,21 @@ const loadImg = function (entries, observer) {
   const [entry] = entries;
   console.log(entry);
   if (!entry.isIntersecting) {
-    return;    
-}else{
-  //replace src with data-source html attributes
-  entry.target.src =entry.target.dataset.src
-  
-  entry.target.addEventListener('load',function(){
-  entry.target.classList.remove('lazy-img');
-})
-observer.unobserve(entry.target); //unobserve the observer to save resources from being logged
-}
+    return;
+  } else {
+    //replace src with data-source html attributes
+    entry.target.src = entry.target.dataset.src;
 
+    entry.target.addEventListener('load', function () {
+      entry.target.classList.remove('lazy-img');
+    });
+    observer.unobserve(entry.target); //unobserve the observer to save resources from being logged
+  }
+};
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
-  // rootMargin:,
+  rootMargin: '-200px',
 });
 imgTargets.forEach(img => imgObserver.observe(img));
 
